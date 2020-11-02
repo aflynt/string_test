4467ef5536f2b116bbf842fa9812c45f83a1c459
flyntga <aflynt1@gmail.com>
2020-11-02 13:38:19 -0600

    coded word sorter

--- t.c
+++ t.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+static int cmpstringp(const void *p1, const void *p2){
+  return strcmp(* (char * const *) p1, * (char * const *) p2);
+}
 void checkalloc(void *ptr){
   if (ptr==NULL){
     perror("bad alloc");
     exit(EXIT_FAILURE);
   }
 }
-char ** resize(char **sa, int cursz, int newsz)
-{
+char ** resize(char **sa, int cursz, int newsz){
   printf("realloc[%d]->[%d]: ",cursz,newsz);
   char **tmp = malloc(newsz*sizeof (char *));
   if (tmp == NULL){
@@ -28,13 +31,34 @@ char ** resize(char **sa, int cursz, int newsz)
   //printf("line [%d]\n",26);
   return tmp;
 }
+void rm_newlines(char * buf){
+  char * sp;
+  for(sp=buf; *sp!= '\0';++sp){
+    if(*sp=='\n'){
+      *sp=' ';
+    }
+  }
+}
+void  print_strings(char **sa,int cnt){
+  for(int i=0;i< cnt;++i){
+      //printf("sa=[%p], &sa[%d]=[%p], sa[%d]=[%p],[%s]\n",sa,i,&sa[i],i,sa[i],sa[i]);
+      //printf("&sa[%d]=[%p], sa[%d]=[%p],[%s]\n",i,&sa[i],i,sa[i],sa[i]);
+      //printf("[%2d]=[%s]\n",i,sa[i]);
+      printf("%s\n",sa[i]);
+  }
+}
+void  free_strings(char **sa,int cnt){
+  for(int i=0;i< cnt;++i){
+      free(sa[i]); sa[i]=NULL;
+  }
+  free(sa); sa=NULL;
+}
 
 int main(int argc, char** argv){
 
-
-  int sz=2;
+  int sz=256;
+  //int sz=2;
   char buf[101];
-  char *str;
   int cnt=0;
   char **sa = malloc(sz*sizeof (char*));
   checkalloc(sa);
@@ -42,37 +66,34 @@ int main(int argc, char** argv){
   char *next=NULL;
   const char delims[] = "., !\n";
 
+  // put a line into the buffer
   while(fgets(buf,100,stdin) != NULL){
-    int len = strlen(buf);
-    for(sp=buf; *sp!= '\0';++sp){
-      if(*sp=='\n'){
-        *sp=' ';
-      }
-    }
+
+    // replace newlines with space
+    rm_newlines(buf);
+
     next=NULL;
     char *token=strtok_r(buf,delims,&next);
     while(token != NULL){
-      int tlen = strlen(token);
-      printf("strlen=%d\n",tlen);
       sp=malloc((strlen(token)+1));
-      //checkalloc(sp);
+      checkalloc(sp);
       strcpy(sp,token);
       sa[cnt++]=sp;
       if(cnt>sz){
         sz*=2;
-//        resize(&sa,cnt-1,sz);
-//        printf("realloc[%d]\n",sz);
-        sa = resize(sa,cnt-1,sz);
-//        sa = realloc(NULL,10);
+        //printf("realloc[%d]\n",sz);
+        char ** tmp = realloc(sa, sz* sizeof (char *));
+        checkalloc(tmp);
+        sa = tmp;
       }
-      printf("[%s]\n",token);
+      //printf("[%2d / %2d] = [%s] \n",cnt,sz,sa[cnt-1]);
       token =strtok_r(NULL,delims,&next);
     }
   }
-  for(int i=0;i< cnt;++i){
-      printf("%p,[%d]:[%s]\n",sa[i],i,sa[i]);
-      free(sa[i]); sa[i]=NULL;
 
-  }
-  free(sa); sa=NULL;
+  // sort the string array
+  qsort(sa, cnt, sizeof(char *), cmpstringp);
+
+  print_strings(sa,cnt);
+  free_strings(sa,cnt);
 }
